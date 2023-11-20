60955c01d5773628d1ce8bde519cb63b1bddb2a2
Wolfie <32025746+Wolfkid200444@users.noreply.github.com>
2023-11-20 09:29:23 -0400

    feat: moved types to different files

---
+++ src/types.d.ts
@@ -0,0 +1,96 @@
+export interface CustomBadges {
+  customBadgesArray: {
+    badge: string;
+    name: string;
+    badges: [
+      {
+        name: string;
+        badge: string;
+      },
+    ];
+  };
+  aliu: {
+    dev: boolean;
+    donor: boolean;
+    contributor: boolean;
+    custom: {
+      url: string;
+      text: string;
+    };
+  };
+  bd: {
+    dev: boolean;
+  };
+  enmity: {
+    supporter: {
+      data: {
+        name: string;
+        id: string;
+        url: {
+          dark: string;
+          light: string;
+        };
+      };
+    };
+    staff: {
+      data: {
+        name: string;
+        id: string;
+        url: {
+          dark: string;
+          light: string;
+        };
+      };
+    };
+    dev: {
+      data: {
+        name: string;
+        id: string;
+        url: {
+          dark: string;
+          light: string;
+        };
+      };
+    };
+    contributor: {
+      data: {
+        name: string;
+        id: string;
+        url: {
+          dark: string;
+          light: string;
+        };
+      };
+    };
+  };
+  goosemod: {
+    sponsor: boolean;
+    dev: boolean;
+    translator: boolean;
+  };
+  vencord: {
+    contributor: boolean;
+    cutie: [
+      {
+        tooltip: string;
+        image: string;
+      },
+    ];
+  };
+  userpfp: string;
+}
+
+export interface BadgeArgs {
+  guildId: string;
+  user: User;
+}
+
+export type BadgeMod = (args: BadgeArgs) => React.ReactElement<{
+  children: React.ReactElement[];
+  className: string;
+}>;
+
+export type BadgeCache = {
+  badges: CustomBadges;
+  lastFetch: number;
+};
